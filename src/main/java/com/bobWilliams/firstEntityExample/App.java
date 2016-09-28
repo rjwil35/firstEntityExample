9d07e428a95fc4f978671d76615dffee4c3c3ad8
Robert Williams <rjwil35@verizon.net>
2016-09-28 16:44:41 -0400

    initial project

---
+++ src/main/java/com/bobWilliams/firstEntityExample/App.java
@@ -0,0 +1,13 @@
+package com.bobWilliams.firstEntityExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
